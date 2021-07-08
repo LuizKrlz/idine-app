61c6fc7437808ebfd6e70c003af62489dea3d0f3
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2021-07-08 07:54:08 -0300

    Initial Commit

---
+++ IDine/IDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDineApp.swift
+//  IDine
+//
+//  Created by luiz on 08/07/21.
+//
+
+import SwiftUI
+
+@main
+struct IDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
